11219ce1bec48f382ac41da02de224abe16602a0
LoneRifle <LoneRifle@users.noreply.github.com>
2020-04-23 23:07:50 +0800

    Fetch restaurants as JSON, append HTML to grid
    - .gitignore node_modules
    - Cleanup index.html
      - Remove reference to missing backtotop.js script
      - Upgrade jQuery to 3.5, ensure it runs first, so that bootstrap
        can take advantage of it
    
    - Drop oboe.js into index.html to fetch JSON file. This will allow
      us to process the JSON file as it comes in, instead of waiting for
      it to completely download
    
    - Implement fetch.js, which fetches `/outlets/index.json` with oboe.js,
      and adds a card for each restaurant received
    - Add a div at the top of the grid in index.html to indicate loading,
      hiding it with fetch.js once we start receiving JSON data
    
    - Move entry for Yum Cha into index.json as a demo
    - Define CSS styles for restaurant cards in our own CSS file, eatcb.css

---
+++ js/fetch.js
@@ -0,0 +1,84 @@
+
+function body (restaurant) {
+  var title = document.createElement('h4')
+  title.setAttribute('class', 'card-title')
+  title.innerText = restaurant.title
+
+  var summary = document.createElement('p')
+  summary.setAttribute('class', 'card-text')
+  summary.innerText = restaurant.summary
+
+  var details = document.createElement('p')
+  details.setAttribute('class', 'card-text')
+  restaurant.body.forEach(function (line) {
+    var l
+    if (line.break) {
+      l = document.createElement('br')
+    } else {
+      l = document.createElement('div')
+      if (line.bold) {
+        boldedText = document.createElement('strong')
+        boldedText.innerText = line.text
+        l.appendChild(boldedText)
+      } else {
+        l.innerText = line.text
+      }
+    }
+    details.appendChild(l)
+  })
+
+  var bodyDiv = document.createElement('div')
+  bodyDiv.setAttribute('class', 'card-body')
+  bodyDiv.appendChild(title)
+  bodyDiv.appendChild(summary)
+  bodyDiv.appendChild(details)
+
+  return bodyDiv
+}
+
+function ava (avatar) {
+  var img = document.createElement('img')
+  img.setAttribute('class', 'img-fluid')
+  img.setAttribute('src', avatar)
+  img.setAttribute('alt', 'Avatar')
+
+  var d = document.createElement('div')
+  d.setAttribute('class', 'card-custom-avatar')
+  d.appendChild(img)
+  return d
+}
+
+function img (image) {
+  var imageDiv = document.createElement('div')
+  imageDiv.setAttribute('class', 'card-custom-img')
+  imageDiv.setAttribute(
+    'style',
+    'background-image: url(' + image + ');'
+  )
+  return imageDiv
+}
+
+
+var grid = document.querySelector('.grid')
+
+oboe('outlets/index.json')
+  .node('restaurants', function () {
+    $(document.getElementById('loading')).hide()
+  })
+  .node('restaurants.*', function (restaurant) {
+    var cardDiv = document.createElement('div')
+    cardDiv.setAttribute(
+      'class', 
+      restaurant.categories.join(' ') +
+      ' card card-custom bg-white border-white border-0'
+    )
+    cardDiv.appendChild(img(restaurant.image))
+    cardDiv.appendChild(ava(restaurant.avatar))
+    cardDiv.appendChild(body(restaurant))
+
+    var card = document.createElement('a')
+    card.setAttribute('href', restaurant.href)
+    card.appendChild(cardDiv)
+    
+    grid.appendChild(card)
+  })
